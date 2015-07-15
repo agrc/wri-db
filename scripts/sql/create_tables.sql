0171704c8f301f5992794f9bc6b8147241c9c27c
steveoh <sgourley@utah.gov>
2015-07-14 18:46:51 -0600

    update sql to WRI db

--- scripts/sql/create_tables.sql
+++ scripts/sql/create_tables.sql
@@ -1,57 +1,64 @@
-DROP TABLE [Wri_Spatial].[dbo].POLY
+DROP TABLE [Wri].[dbo].POLY
 
-CREATE TABLE [Wri_Spatial].[dbo].POLY(
-	[FeatureID] [int] IDENTITY(1,1) NOT NULL,
-    TypeDescription nvarchar(50) NULL,
-    TypeCode int Null,
-	GUID uniqueidentifier NOT NULL,
-	Project_FK uniqueidentifier NOT NULL,
-    Project_ID bigint NOT NULL,
-	StatusDescription varchar(50) NULL,
-	StatusCode int Null,
-	Shape geometry NULL,
-	PRIMARY KEY CLUSTERED
+CREATE TABLE [Wri].[dbo].[POLY](
+	[FeatureID] [bigint] IDENTITY(1,1) NOT NULL,
+	[TypeDescription] [nvarchar](255) NULL,
+	[TypeCode] [int] NULL,
+	[GUID] [uniqueidentifier] NOT NULL,
+	[Project_FK] [uniqueidentifier] NOT NULL,
+	[Project_ID] [bigint] NOT NULL,
+	[StatusDescription] [varchar](50) NULL,
+	[StatusCode] [int] NULL,
+	[Shape] [geometry] NULL,
+	[AreaAcres] [numeric](38, 8) NULL,
+ CONSTRAINT [PK__POLY__82230A2972370709] PRIMARY KEY CLUSTERED
 (
 	[FeatureID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
-DROP TABLE [Wri_Spatial].[dbo].POINT
+DROP TABLE [Wri].[dbo].POINT
 
-CREATE TABLE [Wri_Spatial].[dbo].POINT(
-	FeatureID int IDENTITY(1,1) NOT NULL,
-	TypeDescription nvarchar(50) NULL,
-    TypeCode int Null,
-	SubType nvarchar(50) NULL,
-	Action nvarchar(255) NULL,
-	Description nvarchar(255) NULL,
-	GUID uniqueidentifier NOT NULL,
-	Project_FK uniqueidentifier NOT NULL,
-    Project_ID bigint NOT NULL,
-	StatusDescription varchar(50) NULL,
-	StatusCode int Null,
-	Shape geometry  NULL,
-	PRIMARY KEY CLUSTERED
+CREATE TABLE [Wri].[dbo].[POINT](
+	[FeatureID] [bigint] IDENTITY(1,1) NOT NULL,
+	[TypeDescription] [nvarchar](255) NULL,
+	[TypeCode] [int] NULL,
+	[FeatureSubTypeID] [int] NULL,
+	[FeatureSubTypeDescription] [nvarchar](255) NULL,
+	[ActionID] [int] NULL,
+	[ActionDescription] [nvarchar](255) NULL,
+	[Description] [nvarchar](255) NULL,
+	[GUID] [uniqueidentifier] NOT NULL,
+	[Project_FK] [uniqueidentifier] NOT NULL,
+	[Project_ID] [bigint] NOT NULL,
+	[StatusDescription] [varchar](50) NULL,
+	[StatusCode] [int] NULL,
+	[Shape] [geometry] NULL,
+ CONSTRAINT [PK__POINT__82230A299395AD24] PRIMARY KEY CLUSTERED
 (
 	[FeatureID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
-DROP TABLE [Wri_Spatial].[dbo].LINE
+DROP TABLE [Wri].[dbo].LINE
 
-CREATE TABLE [Wri_Spatial].[dbo].LINE(
-	FeatureID int IDENTITY(1,1) NOT NULL,
-	TypeDescription nvarchar(50) NULL,
-    TypeCode int Null,
-	SubType nvarchar(50) NULL,
-	Action nvarchar(255) NULL,
-	GUID uniqueidentifier NOT NULL,
-	Project_FK uniqueidentifier NOT NULL,
-    Project_ID bigint NOT NULL,
-	StatusDescription varchar(50) NULL,
-	StatusCode int Null,
-	Shape geometry NULL,
-	PRIMARY KEY CLUSTERED
+CREATE TABLE [Wri].[dbo].[LINE](
+	[FeatureID] [bigint] IDENTITY(1,1) NOT NULL,
+	[TypeDescription] [nvarchar](255) NULL,
+	[TypeCode] [int] NULL,
+	[FeatureSubTypeID] [int] NULL,
+	[FeatureSubTypeDescription] [nvarchar](255) NULL,
+	[ActionID] [int] NULL,
+	[ActionDescription] [nvarchar](255) NULL,
+	[Description] [nvarchar](255) NULL,
+	[GUID] [uniqueidentifier] NOT NULL,
+	[Project_FK] [uniqueidentifier] NOT NULL,
+	[Project_ID] [bigint] NOT NULL,
+	[StatusDescription] [varchar](50) NULL,
+	[StatusCode] [int] NULL,
+	[Shape] [geometry] NULL,
+	[LengthFeet] [numeric](12, 2) NULL,
+ CONSTRAINT [PK__LINE__82230A29116B9894] PRIMARY KEY CLUSTERED
 (
 	[FeatureID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
