8f29fdca8a59cb3cb56755ed2fca8df6ea9cf6fc
steveoh <sgourley@utah.gov>
2015-10-27 09:32:57 -0600

    do not orphan records

--- scripts/sql/truncate_spatial_tables.sql
+++ scripts/sql/truncate_spatial_tables.sql
@@ -1,7 +1,17 @@
+truncate table [dbo].[POINT]
+
 truncate table [dbo].[LINE]
 
 truncate table [dbo].[POLY]
 
-truncate table [dbo].[POINT]
+truncate table [dbo].[STREAM]
+
+truncate table [dbo].[FOCUSAREA]
+
+truncate table [dbo].[LANDOWNER]
+
+truncate table [dbo].[SGMA]
+
+truncate table [dbo].[COUNTY]
 
 update [dbo].[Project] set Centroid = NULL
