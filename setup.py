3d9672fd7a48d086d2077ce4ffec53fdecb4d405
steveoh <sgourley@utah.gov>
2015-10-29 11:30:44 -0600

    Release v4.1.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ class Tox(TestCommand):
 
 setup(
     name="wri-dbseeder",
-    version="4.0.1",
+    version="4.1.0",
     license="MIT",
     description="ETL WRI Data",
     long_description="",
